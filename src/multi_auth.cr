28ebb7de51b9cfb47f57e9d475fe51da0507b382
Stephen von Takach <steve@place.technology>
2025-10-13 10:03:31 +1100

    feat: add support for provider factories

--- src/multi_auth.cr
+++ src/multi_auth.cr
@@ -6,9 +6,15 @@ module MultiAuth
   {% end %}
 
   @@configuration = Hash(String, Tuple(String, String)).new
+  @@factories = Hash(String, Proc(String, MultiAuth::Provider)).new
 
   def self.make(provider : String, redirect_uri : String)
-    MultiAuth::Engine.new(provider, redirect_uri)
+    if provider_factory = @@factories[provider]?
+      provider_instance = provider_factory.call(redirect_uri)
+      MultiAuth::Engine.new(provider_instance)
+    else
+      MultiAuth::Engine.new(provider, redirect_uri)
+    end
   end
 
   def self.configuration
@@ -17,5 +23,13 @@ module MultiAuth
 
   def self.config(provider : String, key : String, secret : String)
     @@configuration[provider] = {key, secret}
+  end
+
+  def self.config(provider : String, &builder : Proc(String, MultiAuth::Provider))
+    @@factories[provider] = builder
+  end
+
+  def self.config(provider : String, builder : Proc(String, MultiAuth::Provider))
+    @@factories[provider] = builder
   end
 end
